5fcf4f81b6f18c9671239cf6a68518f62e9898d8
GodwinCyber <godwinchuks032@gmail.com>
2023-12-14 20:19:35 -0800

    Shell sort - Knuth Sequence

---
+++ 100-shell_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * shell_sort -  sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @array: The array to be sorted
+ * @size: The size of array
+ * Return: print array each time the the interval is decresaed
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap, i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	gap = 1;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > temp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = temp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+
+
+}
--- sort.h
+++ sort.h
@@ -20,6 +20,7 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void shell_sort(int *array, size_t size);
 int partition(int *array, size_t size, int low, int high);
 void quicksort(int *array, size_t size, int low, int high);
 void print_array(const int *array, size_t size);
